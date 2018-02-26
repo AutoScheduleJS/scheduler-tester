dda89bcc2e5d7d5122c1ba7dab1688f6f398f899
PBM42 <PBM764927@gmail.com>
2018-02-26 17:39:44 +0100

    feat(react): add on-testbench

--- src/web/react/index.tsx
+++ src/web/react/index.tsx
@@ -2,13 +2,29 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import { coreState$ } from '../../core-state/core.store';
+import { OnTestbenchQueriesUpdateAction } from '../../core-state/on-testbench-queries.reducer';
+// import { OnTestbenchUserstateUpdateAction } from '../../core-state/on-testbench-userstate.reducer';
 
+import onTestbench from './on-testbench';
 import StepOption from './step-option';
 
-const app =
-<div>
+const OnTestbenchQueries = onTestbench(({ onTestbenchQueries, suites }) => ({
+  benchIndex: onTestbenchQueries,
+  benchSuite: suites,
+}));
+
+const app = (
   <div>
-    <StepOption {...{state$: coreState$}} />
+    <div>
+      <StepOption {...{ state$: coreState$ }} />
+    </div>
+    <div>
+      <OnTestbenchQueries
+        {...{ state$: coreState$, actionFn: e => new OnTestbenchQueriesUpdateAction(e) }}
+      >
+        Queries on test bench
+      </OnTestbenchQueries>
+    </div>
   </div>
-</div>;
+);
 ReactDOM.render(app, document.getElementById('app'));
---
+++ src/web/react/on-testbench.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+
+import { ICoreState } from '../../core-state/core.state';
+import { actionTrigger$, actionType } from '../../core-state/core.store';
+
+import { connect } from './util/connect';
+
+interface IState {
+  benchIndex: number;
+  benchSuite: ReadonlyArray<ReadonlyArray<any>>;
+}
+
+interface ICmpProps extends IState {
+  action: (u: actionType) => void;
+  actionFn: (e: string) => actionType;
+}
+
+const cmp: React.SFC<ICmpProps> = ({ benchIndex, benchSuite, action, actionFn, children }) => (
+  <div>
+    <div>{children}</div>
+    <select onChange={e => action(actionFn(e.target.value))}>
+      <option value="empty" />
+      {benchSuite.map(suiteToOptionCmp(benchIndex))}
+    </select>
+  </div>
+);
+
+export default (selector: (s: ICoreState) => IState) => connect(selector, actionTrigger$)(cmp);
+
+const suiteToOptionCmp = (benchIndex: number) => (item: ReadonlyArray<any>, i: number) => (
+  <option value={i} {...getOptionSelected(benchIndex, i)}>
+    SUITE LENGTH: {item.length}
+  </option>
+);
+
+const getOptionSelected = (state: number, itemIndex: number) =>
+  itemIndex === state ? { selected: true } : {};
