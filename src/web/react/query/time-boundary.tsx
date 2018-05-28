cc9edd2e2da3208401a7e7171ac323b282b71c28
PBM42 <PBM764927@gmail.com>
2018-05-28 11:25:19 +0200

    feat: add full timeboundaries for start/end

--- src/web/react/query/time-boundary.tsx
+++ src/web/react/query/time-boundary.tsx
@@ -1,6 +1,5 @@
 import { ITimeBoundary } from '@autoschedule/queries-fn';
 import * as React from 'react';
-
 import ObjectToForm from '../shared/object-to-form';
 import { displayFlex } from '../shared/style.css';
 
@@ -14,7 +13,11 @@ const cmp: React.SFC<ICmpProps> = ({ actionFn, timeBoundary, children }) => (
     <div>{children}</div>
     <ObjectToForm
       value={timeBoundary}
-      labels={[{ key: 'target', label: 'Target' }]}
+      labels={[
+        { key: 'target', label: 'Target' },
+        { key: 'min', label: 'Min' },
+        { key: 'max', label: 'Max' },
+      ]}
       updateFn={v => actionFn(formToObject(v))}
     />
     <button onClick={() => actionFn({ ...defaultValue })}>default</button>
@@ -26,7 +29,7 @@ export default cmp;
 const defaultValue: ITimeBoundary = { min: 1, target: 1, max: 1 };
 
 const formToObject = (form: ITimeBoundary): ITimeBoundary | undefined => {
-  if (!form.target) {
+  if (!form.target && !form.max && !form.min) {
     return undefined;
   }
   return form;
