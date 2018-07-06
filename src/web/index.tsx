db10a0f4187f032f8d9988064a91ea09d8451f56
PBM42 <PBM764927@gmail.com>
2018-07-06 14:46:49 +0200

    feat: add placeholder for results

--- src/web/index.tsx
+++ src/web/index.tsx
@@ -1,10 +1,22 @@
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { css } from 'emotion';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { EditTabsManager } from './edit-tabs-manager';
 import { NewQueryButton } from './new-query';
 import QueriesManager from './queries-manager';
 import { QueryEdit } from './query-edit';
+
+const flexChild = css`
+  flex-basis: 0;
+  flex-grow: 1;
+`;
+
+const flexParent = css`
+  display: flex;
+  flex-direction: column;
+  height: 90%;
+`;
 
 const app = (
   <div>
@@ -13,10 +25,17 @@ const app = (
         <Typography variant="title">Scheduler Lab</Typography>
       </Toolbar>
     </AppBar>
-    <EditTabsManager labels={['Queries manager', 'User-state manager']}>
-      <QueriesManager />
-      <span>Toto</span>
-    </EditTabsManager>
+    <div className={flexParent}>
+      <div className={flexChild}>
+        <EditTabsManager labels={['Queries manager', 'User-state manager']}>
+          <QueriesManager />
+          <span>Toto</span>
+        </EditTabsManager>
+      </div>
+      <div className={flexChild}>
+        <p>Test</p>
+      </div>
+    </div>
     <NewQueryButton />
     <QueryEdit />
   </div>
